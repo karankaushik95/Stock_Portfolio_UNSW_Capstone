6d2d3979e0435460af964766bd42c6aeb2ca317b
keegan-gyoery <keegan.gyoery@gmail.com>
2019-10-27 14:21:35 +1100

    Merge issue

--- runlocal.ps1
+++ runlocal.ps1
@@ -1,8 +1,16 @@
 # Set-ExecutionPolicy AllSigned
 
-env\Scripts\activate
+$env_path="env"
+New-Item -ItemType Directory -Force -Path $env_path
+python -m virtualenv .\env
+.\env\Scripts\activate.bat
+.\env\Scripts\activate
 
 pip -q install --upgrade pip
-pip -q install -r ..\requirements.txt
+pip -q install -r requirements.txt
+
+$env:PROJ_CONFIG="config\application-dev.cfg"
+$env:FLASK_APP="backend\server.py"
+$env:FLASK_DEBUG=1
 
 flask run
