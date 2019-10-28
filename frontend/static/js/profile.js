0932a08d133207fc79eb1a3a649798e2c6a2cebd
Karan Kaushik <kkricardokaka95@gmail.com>
2019-10-28 22:00:47 +1100

    Added jquery for page within page

--- frontend/static/js/profile.js
+++ frontend/static/js/profile.js
@@ -1,13 +1,13 @@
-async function fetchHtmlAsText(url) {
-    console.log("async triggered");
-    return await (await fetch(url)).text();
-}
+// async function fetchHtmlAsText(url) {
+//     console.log("async triggered");
+//     return await (await fetch(url)).text();
+// }
 
-function loadhome() {
-    document.getElementById("profile").addEventListener("click", async function (event) {
-        event.preventDefault();
-        document.getElementById("dashboard_heading").innerHTML = await fetchHtmlAsText("../index.html");
-    });
-}
+// function loadhome() {
+//     document.getElementById("profile").addEventListener("click", async function (event) {
+//         event.preventDefault();
+//         document.getElementById("dashboard_heading").innerHTML = await fetchHtmlAsText("../index.html");
+//     });
+// }
 
-loadhome();
+// loadhome();
