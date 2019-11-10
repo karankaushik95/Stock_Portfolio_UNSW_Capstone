81355877920404a0e4b849b3bcf1e0836af565d6
keegan-gyoery <keegan.gyoery@gmail.com>
2019-11-10 17:00:22 +1100

    Few little fixes

--- backend/modules/watchlist/watchlist_s_worker.py
+++ backend/modules/watchlist/watchlist_s_worker.py
@@ -2,6 +2,7 @@ import sqlite3
 from datetime import date
 import time
 
+
 class Watchlist():
 
     @staticmethod
@@ -24,27 +25,26 @@ class Watchlist():
         sql = f""" UPDATE watchlist set status = 0 where user_id = {user_id} AND static_id = {static_id};"""
         cursor.execute(sql)
         connection.commit()
-        connection.close()      
+        connection.close()
 
     @staticmethod
     def show_watchlist(user_id):
         connection = sqlite3.connect('watchlist.db')
-        cursor = connection.cursor()   
+        cursor = connection.cursor()
         sql = f'SELECT * from watchlist where user_id = {user_id} AND status = 1'
         cursor.execute(sql)
         results = cursor.fetchall()
         connection.commit()
-        connection.close()  
+        connection.close()
 
         reply = []
         for each_result in results:
-            reply.append({'WATCHLIST_ID': each_result[0], 
+            reply.append({'WATCHLIST_ID': each_result[0],
                           'USER_ID': each_result[1],
                           'STATIC_ID': each_result[2],
                           'STATUS': each_result[3],
                           'DATE': each_result[4],
-                          'TIME': each_result[5]
-                        })
+                          'TIME': each_result[5]})
         return reply
 
 # Watchlist.new_stock(1,2)
