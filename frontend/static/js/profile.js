3a10f5a00ba063bc42c389950dcf133e0ec8a1f2
Karan Kaushik <kkricardokaka95@gmail.com>
2019-11-12 10:53:40 +1100

    Oopsies

--- frontend/static/js/profile.js
+++ frontend/static/js/profile.js
@@ -1,11 +1,14 @@
-window.addEventListener("load", (event)=>{
+window.addEventListener("load", (event) => {
     document.getElementById("profile-username").innerText = sessionStorage.getItem("email");
     document.getElementById("profile-fullname").innerText = sessionStorage.getItem("name");
     console.log("HERE");
 });
 
-window.onload = ()=>{
+window.onload = () => {
     document.getElementById("profile-username").innerText = sessionStorage.getItem("email");
     document.getElementById("profile-fullname").innerText = sessionStorage.getItem("name");
     console.log("HERE");
 };
+
+document.getElementById("profile-username").innerText = sessionStorage.getItem("email");
+document.getElementById("profile-fullname").innerText = sessionStorage.getItem("name");
