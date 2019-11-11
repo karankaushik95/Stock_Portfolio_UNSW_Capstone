c3d105d8f5d3cef970241a7ab15e47d9b6020ff0
Karan Kaushik <kkricardokaka95@gmail.com>
2019-11-11 12:32:08 +1100

    Merged?

--- frontend/static/js/byteme.js
+++ frontend/static/js/byteme.js
@@ -125,57 +125,6 @@ document.getElementById("forgotPassSubmit").addEventListener("click", function (
 document.getElementById("loginButton").addEventListener("click", function (event) {
 
     event.preventDefault();
-<<<<<<< HEAD
-    const username = document.getElementById("username");
-    const usernameSpan = document.getElementById("username_error");
-
-    const password = document.getElementById("password");
-    const passwordSpan = document.getElementById("password_error");
-    passwordSpan.style.color = "red";
-    usernameSpan.style.color = "red";
-
-    if (!password.value.trim()) {
-        passwordSpan.innerText = "Please enter your password";
-        password.focus();
-        password.style.borderColor = "red";
-    } else {
-        password.style.borderColor = "";
-        passwordSpan.innerText = "";
-    }
-    if (!username.value.trim()) {
-        username.style.borderColor = "red";
-        username.focus();
-        usernameSpan.innerText = "Please enter a username";
-    } else {
-        usernameSpan.innerText = "";
-        username.style.borderColor = "";
-    }
-    if (username.value && password.value) {
-        //call API
-        let formData = new FormData();
-        formData.append("username", username.value);
-        formData.append("password", password.value);
-        console.log(username.value);
-        console.log(password.value);
-        fetch(apiUrl + "/login", {
-            method: 'POST',
-            body: formData
-        })
-        .then(resp => console.log(resp))
-        //.then(data => console.log(data));
-        // {
-        //     console.log(response);
-        //     // if (response["success"] === "false") {
-        //     //     username.style.borderColor = "red";
-        //     //     username.focus();
-        //     //     passwordSpan.innerText = "Please check your credentials and try again";
-        //     //     password.focus();
-        //     //     password.style.borderColor = "red";
-        //     //     return;
-        //     // }
-        // });
-    }
-=======
     window.location.href = "/login.html";
 
     // const username = document.getElementById("username");
@@ -223,7 +172,6 @@ document.getElementById("loginButton").addEventListener("click", function (event
         
     //     //alert("Valid");
     // }
->>>>>>> master
 });
 
 document.getElementById("registerButton").addEventListener("click", function (event) {
