d62515a495eafcf56852e841bb63cc3fad683a43
keegan-gyoery <keegan.gyoery@gmail.com>
2019-11-26 16:59:23 +1100

    setup touch ups

--- backend/setup.py
+++ backend/setup.py
@@ -6,12 +6,18 @@ import sys
 def bootstrap():
     if not os.path.exists('db/users.db'):
         print("The file db/users.db does not exist.", file=sys.stderr)
-        print("Run db/user_setup.py before running the server.", file=sys.stderr)
+        print("Run the runlocal script again.", file=sys.stderr)
         print("System exiting.", file=sys.stderr)
         sys.exit(1)
 
     if not os.path.exists('db/users'):
         print("The folder db/users does not exist.", file=sys.stderr)
-        print("Create the folder before running the server.", file=sys.stderr)
+        print("Run the runlocal script again.", file=sys.stderr)
+        print("System exiting.", file=sys.stderr)
+        sys.exit(1)
+
+    if not os.path.exists('sqlite/'):
+        print("The folder slite does not exist.", file=sys.stderr)
+        print("Download the sqlite tools using the sqlite3_setup script.", file=sys.stderr)
         print("System exiting.", file=sys.stderr)
         sys.exit(1)
